afe74593c9f7f468df93b903f26e168d4d892781
Tom Banish <tombanish@gmail.com>
2020-06-10 17:45:52 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -21,9 +21,10 @@ class InteractiveRecord
     column_names.compact
   end
   
-  self.column_names.each do |col_name|
-    attr_accessor col_name.to_sym
-  end
+  def attr_accessor
+    self.column_names.each do |col_name|
+      attr_accessor col_name.to_sym
+    end
   
   def initialize(options={})
     options.each do |property, value|
